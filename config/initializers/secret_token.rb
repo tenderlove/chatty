13ff2b95b0229aad3e67979a84fd369eadaf1966
Aaron Patterson <aaron.patterson@gmail.com>
2012-07-21 15:35:31 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# Make sure your secret_token is kept private
+# if you're sharing your code publicly.
+Chatty::Application.config.secret_token = '7882dedb4e9f53f53384ddd1e94641cd2129e73ca60477aaf6979d25f30e32925f9024bc1a2e0a9d6e2684e5327a0324c2fd011f77299d0b93272c319248e652'
