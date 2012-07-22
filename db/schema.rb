15ef5bd987995c656a66f7b87254244bd2efa028
Aaron Patterson <aaron.patterson@gmail.com>
2012-07-21 19:43:15 -0700

    adding messages resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120722024145) do
+
+  create_table "messages", :force => true do |t|
+    t.string   "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
